fc7ab4ad14234738d0841af7fb1553613b6bd3a3
Student <student@iMAC-5.local>
2024-11-11 09:17:52 +0100

    Dodavanje 7.koraka

---
+++ Ruap_lv1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ruap_lv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
